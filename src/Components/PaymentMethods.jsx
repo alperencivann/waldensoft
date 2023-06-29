5418e155d4b05600d9fe04c1e7f7c713a950071d
Alperen Civan <alperencivan831@hotmail.com>
2023-06-29 11:59:07 +0300

    silindi

--- src/Components/PaymentMethods.jsx
+++ src/Components/PaymentMethods.jsx
@@ -4,8 +4,6 @@ const PaymentMethods = () => {
     return (
         <div className="w-full bg-stone-900 text-white text-center p-24 h-56">
             Payment Methods
-            buraya ekleme yapılmıştır
-            bu alana tekrar ekleme yapılmıytır.
         </div>
     );
 };
