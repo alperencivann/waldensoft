d12dd3bb57deeafe52d5bf2a6da82c76cf9cd375
Alperen Civan <alperencivan831@hotmail.com>
2023-06-29 11:53:28 +0300

    ekledim iste

--- src/Components/PaymentMethods.jsx
+++ src/Components/PaymentMethods.jsx
@@ -5,6 +5,7 @@ const PaymentMethods = () => {
         <div className="w-full bg-stone-900 text-white text-center p-24 h-56">
             Payment Methods
             buraya ekleme yapılmıştır
+            bu alana tekrar ekleme yapılmıytır.
         </div>
     );
 };
